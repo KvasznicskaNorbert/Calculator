8952c78d4c04256875936234707270124c57958a
KvasznicsakNorbert <kvasznicska.norbert@gmail.com>
2023-02-21 11:07:17 +0100

    bootstrap include

--- index.php
+++ index.php
@@ -54,32 +54,36 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Calculator</title>
 </head>
 <body>
+    <div class="container m-5 text-center">
+        <form method="post" class="m-4">
+            <input type="number" name="value1" step="0.01" id="value1" value="<?php if(isset($_SESSION["result"])){print($_SESSION["result"]);} ?>">
+            <select name="operation" id="operation">
+                <option value="add">Add</option>
+                <option value="subtraction">Subtraction</option>
+                <option value="multiplication">Multiplication</option>
+                <option value="division">Division</option>
+            </select>
+            <input type="number" name="value2" step="0.01" id="value2">
+            <input type="submit" name="ok" value="Calculate">
+            <input type="submit" name="deleteHistory" value="Delete History">
+        </form>
 
-    <form method="post">
-        <input type="number" name="value1" id="value1" value="<?php if(isset($_SESSION["result"])){print($_SESSION["result"]);} ?>">
-        <select name="operation" id="operation">
-            <option value="add">Add</option>
-            <option value="subtraction">Subtraction</option>
-            <option value="multiplication">Multiplication</option>
-            <option value="division">Division</option>
-        </select>
-        <input type="number" name="value2" id="value2">
-        <input type="submit" name="ok" value="Calculate">
-    </form>
-    <form method="post">
-    <input type="submit" name="deleteHistory" value="Delete History">
-    </form>
-
-    <?php
-        if(isset($result)){
-            foreach($_SESSION["history"] as $history){
-                print($history);
+        <?php
+            if(isset($result)){
+                foreach($_SESSION["history"] as $history){
+                    print($history);
+                } 
             } 
-        } 
-    ?>
+        ?>
+
+        <div>
+
+        </div>
+    </div>
     
 </body>
 </html>
